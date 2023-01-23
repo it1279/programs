a12cefa96469cf5eac47b7a6f90b1e2b461285b2
it1279 <120377606+it1279@users.noreply.github.com>
2023-01-23 16:12:48 +0530

    Add files via upload

---
+++ arithematic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter the values");
+scanf("%d%d",&a,&b);
+printf("%d\n",a+b);
+printf("%d\n",a-b);
+printf("%d\n",a*b);
+printf("%d\n",a/b);
+printf("%d",a%b);
+return;
+}
