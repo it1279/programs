d956543308cf5d4795ac4a596352d49ea2b347bc
it1279 <120377606+it1279@users.noreply.github.com>
2023-01-23 16:12:09 +0530

    Add files via upload

---
+++ area_of_rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int length,breadth,area_of_rectangle;
+printf("enter length\n");
+
+scanf("%d",&length);
+printf("enter breadth\n");
+scanf("%d",&breadth);
+area_of_rectangle=length*breadth;
+printf("area of retangle %d",area_of_rectangle);
+return;
+}
