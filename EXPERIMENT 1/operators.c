1daee580b03e42c529bf964b761c101e36967f90
it1279 <120377606+it1279@users.noreply.github.com>
2023-01-30 16:10:27 +0530

    Add files via upload

---
+++ EXPERIMENT 1/operators.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int a=5,b=4;
+
+printf("%d\n",a+b);
+printf("%d\n",a-b);
+printf("%d\n",a*b);
+printf("%d\n",a/b);
+printf("%d\n",a%b);
+printf("%d\n",--a);
+printf("%d\n",--b);
+printf("%d\n",++a);
+printf("%d\n",++b);
+printf("%d\n",a==b);
+printf("%d\n",a>=b);
+printf("%d\n",a<=b);
+printf("%d\n",a!=b);
+printf("%d\n",a&&b);
+printf("%d\n",!a); 
+printf("%d\n",a||b);
+
+return;
+}
